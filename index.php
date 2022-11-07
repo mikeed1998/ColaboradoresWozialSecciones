c78a4c3699b546df981a01cb0d7e7c00dd207545
Michael <mikeed1998@gmail.com>
2022-11-07 17:00:36 -0600

    esqueleto1

---
+++ index.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>APF</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container-fluid border border-dark">
+        <div class="row border border-dark">
+            <div class="col-md-2 border border-dark">
+
+            </div>
+            <div class="col-md-8 border border-dark">
+                <div class="row border border-primary">
+                    <div class="col-md-3 border border-primary">
+                        HOME
+                    </div>
+                    <div class="col-md-3 border border-primary">
+                        NOSOTROS
+                    </div>
+                    <div class="col-md-3 border border-primary">
+                        DIRECTORIO
+                    </div>
+                    <div class="col-md-3 border border-primary">
+                        CONTACTO
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-1 border border-dark">
+                TEL. 3338096501
+            </div>
+            <div class="col-md-1 border border-dark">
+
+            </div>
+        </div>
+        <div class="row border border-dark">
+            <div class="col border border-primary mt-2 mb-2">
+                Carrusel
+            </div>
+        </div>
+        <div class="row px-5">
+            <div class="col-md-4 mt-2 mb-2" style="border-style: dotted; border-color: blue;">
+                IMAGEN
+            </div>
+            <div class="col-md-8 mt-2 mb-2" style="border-style: dotted; border-color: blue;">
+                <h1>TITULO AQUI</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus sequi voluptate reprehenderit! Iusto temporibus culpa sunt minus, quas tempora ipsum, ut fugit expedita rerum enim veritatis, sint debitis at accusantium? Illum quis dolore quae ab laboriosam, ullam quos modi eius.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque minima autem rem vel? Placeat, harum?</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-2 mx-auto text-center" style="border-style: dotted; border-color: black;">
+                <h1>TITULO AQUI</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-9 mx-auto" style="border-style: dotted; border-color: blue;">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi in quae sint suscipit temporibus. Tempora quasi modi dicta dolorum delectus iste facere quibusdam voluptatibus ut. Quia ullam quos eos. Molestiae?
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-2 py-5 mx-auto text-center" style="border-style: dotted; border-color: blue;">
+                LOGO
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-6 py-1 mx-auto text-center" style="border-style: dotted; border-color: blue;">
+                <div class="row">
+                    <div class="col-md-3 py-5" style="border-style: dotted; border-color: red;">
+                        CIRCULO
+                    </div>
+                    <div class="col-md-3 py-5" style="border-style: dotted; border-color: red;">
+                        CIRCULO
+                    </div>
+                    <div class="col-md-3 py-5" style="border-style: dotted; border-color: red;">
+                        CIRCULO
+                    </div>
+                    <div class="col-md-3 py-5" style="border-style: dotted; border-color: red;">
+                        CIRCULO
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+</body>
+</html>
