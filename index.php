1037902b2c34dcbef6869c00555b5f0b74193672
Michael <mikeed1998@gmail.com>
2022-11-08 15:30:13 -0600

    design avanzado

--- index.php
+++ index.php
@@ -6,97 +6,213 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>APF</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" type="text/css" href="vendor/slick/slick.css"/>
+    <link rel="stylesheet" type="text/css" href="vendor/slick/slick-theme.css"/>
+    <style>
+        .form-control::placeholder {
+            color: white;
+        }
+
+        .form-control:focus {
+            background-color: black;
+            color: white;
+            outline: 0 !important;
+        }
+    </style>
 </head>
 <body>
-    <div class="container-fluid border border-dark">
-        <div class="row border border-dark">
-            <div class="col-md-2 border border-dark">
-
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-2 text-center">
+                <img src="img/APF_LOGO.png" alt="img-fluid" width="50%">
             </div>
-            <div class="col-md-8 border border-dark">
-                <div class="row border border-primary">
-                    <div class="col-md-3 border border-primary">
-                        HOME
+            <div class="col-md-8">
+                <div class="row bg-black text-white">
+                    <div class="col-md-3 py-3 text-center">
+                        <a href="#" class="btn outline-link text-white" style="decoration: none;"><h5>HOME</h5></a>
                     </div>
-                    <div class="col-md-3 border border-primary">
-                        NOSOTROS
+                    <div class="col-md-3 py-3">
+                        <a href="#" class="btn outline-link text-white" style="decoration: none;"><h5>NOSOTROS</h5></a>
                     </div>
-                    <div class="col-md-3 border border-primary">
-                        DIRECTORIO
+                    <div class="col-md-3 py-3">
+                        <a href="#" class="btn outline-link text-white" style="decoration: none;"><h5>DIRECTORIO</h5></a>
                     </div>
-                    <div class="col-md-3 border border-primary">
-                        CONTACTO
+                    <div class="col-md-3 py-3">
+                        <a href="#" class="btn outline-link text-white" style="decoration: none;"><h5>CONTACTO</h5></a>
                     </div>
                 </div>
             </div>
-            <div class="col-md-1 border border-dark">
-                TEL. 3338096501
+            <div class="col-md-1 py-4 text-center">
+                <h5>TEL. 3338096501</h5>
             </div>
-            <div class="col-md-1 border border-dark">
-
+            <div class="col-md-1 py-4">
+                <a href="#" class="btn outline-link p-0 shadow-none" style="decoration: none;">
+                    <i class="fa-brands fa-whatsapp fa-2xl px-1"></i>
+                </a>
+                <a href="#" class="btn outline-link p-0" style="decoration: none;">
+                    <i class="fa-brands fa-facebook-f fa-2xl px-1"></i>
+                </a>
+                <a href="#" class="btn outline-link p-0" style="decoration: none;">
+                    <i class="fa-brands fa-instagram fa-2xl px-1"></i>
+                </a>
             </div>
         </div>
-        <div class="row border border-dark">
-            <div class="col border border-primary mt-2 mb-2">
-                Carrusel
+    </div>
+    <div class="container-fluid">
+        <div class="row">
+            <div id="carrusel">
+                <div>
+                    <img src="img/KIDS.jpg" alt="img-fluid" width="100%">
+                </div>
+                <div>
+                    <img src="img/45.jpg" alt="img-fluid" width="">
+                </div>
             </div>
         </div>
         <div class="row px-5">
-            <div class="col-md-4 mt-2 mb-2" style="border-style: dotted; border-color: blue;">
-                IMAGEN
+            <div class="col-md-4 mt-2 mb-2 px-1">
+                <img src="img/45.jpg" alt="">
             </div>
-            <div class="col-md-8 mt-2 mb-2" style="border-style: dotted; border-color: blue;">
-                <h1>TITULO AQUI</h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus sequi voluptate reprehenderit! Iusto temporibus culpa sunt minus, quas tempora ipsum, ut fugit expedita rerum enim veritatis, sint debitis at accusantium? Illum quis dolore quae ab laboriosam, ullam quos modi eius.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque minima autem rem vel? Placeat, harum?</p>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-md-2 mx-auto text-center" style="border-style: dotted; border-color: black;">
-                <h1>TITULO AQUI</h1>
+            <div class="col-md-8 mt-2 mb-2 px-5">
+                <h1 class="display-4">TITULO AQUI</h1>
+                <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus sequi voluptate reprehenderit! Iusto temporibus culpa sunt minus, quas tempora ipsum, ut fugit expedita rerum enim veritatis, sint debitis at accusantium? Illum quis dolore quae ab laboriosam, ullam quos modi eius.</h4>
+                <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, voluptas iste aspernatur amet laborum laboriosam veniam neque, aut inventore non veritatis voluptatem. Nesciunt quia a pariatur obcaecati quibusdam id tenetur deserunt quidem facere ratione reiciendis provident, vero, ipsam magni nam?</h4>
+                <h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque minima autem rem vel? Placeat, harum?</h4>
             </div>
         </div>
-        <div class="row">
-            <div class="col-md-9 mx-auto" style="border-style: dotted; border-color: blue;">
+        <div class="row mt-5" style="background: #FAFAFA;">
+            <div class="col-md-2 mx-auto text-center">
+                <h1 class="display-5">TITULO AQUI</h1>
+            </div>
+        </div>
+        <div class="row" style="background: #FAFAFA;">
+            <div class="col-md-9 mx-auto">
+            <h4>                
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi in quae sint suscipit temporibus. Tempora quasi modi dicta dolorum delectus iste facere quibusdam voluptatibus ut. Quia ullam quos eos. Molestiae?
+            </h4>
             </div>
         </div>
-        <div class="row">
-            <div class="col-md-2 py-5 mx-auto text-center" style="border-style: dotted; border-color: blue;">
-                LOGO
+        <div class="row" style="background: #FAFAFA;">
+            <div class="col-md-2 py-5 mx-auto text-center">
+                <img src="img/APF_LOGO.png" alt="img-fluid" width="50%">
             </div>
         </div>
-        <div class="row mb-5 border border-dark">
-            <div class="col-md-6 py-1 mx-auto text-center" style="border-style: dotted; border-color: blue;">
-                <div class="row">
+        <div class="row mb-5" style="background: #FAFAFA;">
+            <div class="col-md-6 py-1 mx-auto text-center">
+                <div class="row mb-5">
                     <div class="col-md-3 py-5" style="border-style: dotted; border-color: red;">
-                        CIRCULO
+                        <img src="..." class="rounded-circle bg-warning py-5 px-5" alt="...">
                     </div>
                     <div class="col-md-3 py-5" style="border-style: dotted; border-color: red;">
-                        CIRCULO
+                        <img src="..." class="rounded-circle bg-warning py-5 px-5" alt="...">
                     </div>
                     <div class="col-md-3 py-5" style="border-style: dotted; border-color: red;">
-                        CIRCULO
+                        <img src="..." class="rounded-circle bg-warning py-5 px-5" alt="...">
                     </div>
                     <div class="col-md-3 py-5" style="border-style: dotted; border-color: red;">
-                        CIRCULO
+                        <img src="..." class="rounded-circle bg-warning py-5 px-5" alt="...">
                     </div>
                 </div>
             </div>
         </div>
-        <div class="row mt-5 border border-dark">
-            <div class="col-12 text-center" style="border-style: dotted; border-color: blue;">
-                newsletter
+        <div class="row mt-5">
+            <div class="col-12 text-center">
+                <h2>newsletter</h2>
             </div>
             <div class="col-12">
-                <div class="col-md-4 mx-auto">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, iusto? Lorem ipsum dolor sit amet.
+                <div class="col-md-6 mx-auto text-center">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, iusto? Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, harum.
                 </div>
             </div>
-            
+        </div>
+        <div class="row mt-5">
+            <div class="col-md-5 mx-auto">
+                <div class="row">
+                    <div class="col-md-4 mx-auto">
+                        <input type="text" class="form-control bg-black" placeholder="Nombre: ">
+                    </div>
+                    <div class="col-md-4 mx-auto">
+                        <input type="text" class="form-control bg-black" placeholder="Correo: ">
+                    </div>
+                    <div class="col-md-4 mx-auto">
+                        <input type="text" class="form-control bg-black" placeholder="whatsapp">
+                    </div>
+                </div>
+                <div class="row mt-3">
+                    <div class="col-md-3 mx-auto">
+                        <input type="submit" class="form-control" style="background-color: red; color: black;">
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 
+    <footer class="footer mt-5 bg-black">
+        <div class="container-fluid bg-black py-3">
+            <div class="row">
+                <div class="col-md-11 mt-5 mx-auto border border-white p-1">
+                    <div class="row mt-5">
+                        <div class="col-md-6 px-5 text-white text-start">
+                            <div class="row">
+                                <div class="col-12">
+                                    <p class="display-6">Home</p>
+                                </div>
+                                <div class="col-12">
+                                    <p class="display-6">DIRECTORIO</p>
+                                </div>
+                                <div class="col-12">
+                                    <p class="display-6">Contacto</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6 text-end text-secondary">
+                            <div class="row mt-5">
+                                <div class="col-12 mt-5 px-5">
+                                    Avenida Lapizlazuli2074 Int3<br>
+                                    Residencial Vicoria, Guadalajara<br>
+                                    jalisco, México.<br>
+                                    contacto@rlc.com<br>
+                                    3338096501.<br>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row mt-2 px-5">
+                        <div class="col-md-12 mx-auto border border-white"></div>
+                    </div>
+                    <div class="row mt-3 mb-3">
+                        <div class="col-md-6 px-5 text-white text-start">
+                            aviso de privacidad<br>
+                            preguntas frecuentes
+                        </div>
+                        <div class="col-md-6 px-5 py-2 text-end">
+                            <i class="fa-brands fa-whatsapp fa-2xl px-2" style="color: red;"></i>
+                            <i class="fa-brands fa-facebook-f fa-2xl px-2" style="color: red;"></i>
+                            <i class="fa-brands fa-instagram fa-2xl px-2" style="color: red;"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4 py-2 mx-auto text-secondary">
+                    deporte y mas 2022 todos los derechos reservados diseño por wozial
+                </div>
+            </div>
+        </div>
+    </footer>
+    <script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+    <script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+    <script type="text/javascript" src="vendor/slick/slick.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    <script>
+        $('#carrusel').slick({
+            dots: true,
+            infinite: true,
+            speed: 300,
+            slidesToShow: 1,
+            adaptiveHeight: true
+        });
+    </script>
 </body>
 </html>
